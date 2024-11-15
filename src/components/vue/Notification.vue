19a168e92c4ecc39b53496f97657810c4ac8dbb9
Valentina210504 <vprados@ufpso.edu.co>
2024-11-15 17:27:39 -0500

    clase 1 vue boton+-

---
+++ src/components/vue/Notification.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import {ref} from 'vue';
+    let qty = ref(1)
+
+    function add() {
+        qty.value++
+        console.log(qty)
+    }
+    function subtract() {
+        qty.value--
+        console.log(qty)
+    }
+</script>
+
+<template>
+    <div class="max-w-80 mx-auto p-6 bg-purple-300 rounded flex gap-6">
+        <div 
+        v-if="qty > 0"
+        class="bg-red-500 text-white size-8 rounded-full flex justify-center items-center font-bold" :class="{'bg-orange-500': qty >3}">
+            {{ qty }}
+        </div>
+
+        <div v-else>nadie te visita :(</div>
+
+        <div 
+        @click="add" 
+        class="bg-blue-400 text-white rounded size-6 flex justify-center items-center">
+            +
+        </div>
+        <div 
+        @click="subtract" 
+        class="bg-blue-400 text-white rounded size-6 flex justify-center items-center">
+            -
+        </div>
+    </div>
+</template>
+<!-- v-if mostar y v-show no mostrar -->
